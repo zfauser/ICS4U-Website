6edd1f22c351b73a78b37c083de0051c59a05fb4
Zach <19974370+Zfauser@users.noreply.github.com>
2024-02-06 09:19:36 -0500

    finish base site

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let homeButton = document.querySelector('#home');
 let aboutButton = document.querySelector('#info');
+let assignmentButton = document.querySelector('#assignments');
+let homeButtonText = document.querySelector('#homeText');
+let aboutButtonText = document.querySelector('#aboutmeText');
+let assignmentButtonText = document.querySelector('#assignmentsText');
 
 homeButton.addEventListener('click', () => {
   window.location.href = 'index.html';
@@ -7,4 +11,32 @@ homeButton.addEventListener('click', () => {
 
 aboutButton.addEventListener('click', () => {
   window.location.href = 'about-me.html';
+});
+
+assignmentButton.addEventListener('click', () => {
+  window.location.href = 'assignments.html';
+});
+
+homeButton.addEventListener("mouseover", () => {
+  homeButtonText.classList.remove('hide');
+});
+
+homeButton.addEventListener("mouseout", () => {
+  homeButtonText.classList.add('hide');
+});
+
+aboutButton.addEventListener("mouseover", () => {
+  aboutButtonText.classList.remove('hide');
+});
+
+aboutButton.addEventListener("mouseout", () => {
+  aboutButtonText.classList.add('hide');
+});
+
+assignmentButton.addEventListener("mouseover", () => {
+  assignmentButtonText.classList.remove('hide');
+});
+
+assignmentButton.addEventListener("mouseout", () => {
+  assignmentButtonText.classList.add('hide');
 });
