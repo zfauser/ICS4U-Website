2d8a757ff7d9d9e5df985c6a124f187ca26067b3
Zach <19974370+Zfauser@users.noreply.github.com>
2024-02-09 20:26:23 -0500

    final

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
-let homeButton = document.querySelector('#home');
-let aboutButton = document.querySelector('#info');
-let assignmentButton = document.querySelector('#assignments');
-let homeButtonText = document.querySelector('#homeText');
-let aboutButtonText = document.querySelector('#aboutmeText');
-let assignmentButtonText = document.querySelector('#assignmentsText');
+let homeButton = document.querySelector("#home");
+let aboutButton = document.querySelector("#info");
+let assignmentButton = document.querySelector("#assignments");
+let homeButtonText = document.querySelector("#homeText");
+let aboutButtonText = document.querySelector("#aboutmeText");
+let assignmentButtonText = document.querySelector("#assignmentsText");
 
-homeButton.addEventListener('click', () => {
-  window.location.href = 'index.html';
+homeButton.addEventListener("click", () => {
+  window.location.href = "index.html";
 });
 
-aboutButton.addEventListener('click', () => {
-  window.location.href = 'about-me.html';
+aboutButton.addEventListener("click", () => {
+  window.location.href = "about-me.html";
 });
 
-assignmentButton.addEventListener('click', () => {
-  window.location.href = 'assignments.html';
+assignmentButton.addEventListener("click", () => {
+  window.location.href = "assignments.html";
 });
 
 homeButton.addEventListener("mouseover", () => {
-  homeButtonText.classList.remove('hide');
+  homeButtonText.classList.remove("hide");
 });
 
 homeButton.addEventListener("mouseout", () => {
-  homeButtonText.classList.add('hide');
+  homeButtonText.classList.add("hide");
 });
 
 aboutButton.addEventListener("mouseover", () => {
-  aboutButtonText.classList.remove('hide');
+  aboutButtonText.classList.remove("hide");
 });
 
 aboutButton.addEventListener("mouseout", () => {
-  aboutButtonText.classList.add('hide');
+  aboutButtonText.classList.add("hide");
 });
 
 assignmentButton.addEventListener("mouseover", () => {
-  assignmentButtonText.classList.remove('hide');
+  assignmentButtonText.classList.remove("hide");
 });
 
 assignmentButton.addEventListener("mouseout", () => {
-  assignmentButtonText.classList.add('hide');
+  assignmentButtonText.classList.add("hide");
 });
 
-if (window.location.href.includes('index.html') || window.location.pathname === '/') {
-  homeButton.classList.add('hide');
-} else if (window.location.href.includes('about-me.html')) {
-  aboutButton.classList.add('hide');
-} else if (window.location.href.includes('assignments.html')) {
-  assignmentButton.classList.add('hide');
+if (
+  window.location.href.includes("index.html") ||
+  window.location.pathname === "/"
+) {
+  homeButton.classList.add("hide");
+} else if (window.location.href.includes("about-me.html")) {
+  aboutButton.classList.add("hide");
+} else if (window.location.href.includes("assignments.html")) {
+  assignmentButton.classList.add("hide");
 }
